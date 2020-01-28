7270d9e2010e2cd8a61d83b51997e631130fae8b
ll-kuma-ll <ll.kuma.o.ll@gmail.com>
2020-01-29 02:57:18 +0900

    Make input type is text class

---
+++ src/InputText.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace LLkumaLL\FormView;
+
+/**
+ * <input type="text">
+ *
+ */
+class InputText extends Input implements Contracts\InputText
+{
+    use Foundations\InputGroup;
+
+    /**
+     * type属性取得
+     *
+     * @return string
+     */
+    public function getTypeAttribute(): string
+    {
+        return property_exists($this, 'type') ? $this->type : 'text';
+    }
+
+    /**
+     * プレースホルダー属性取得
+     *
+     * @return string
+     */
+    public function getPlaceholderAttribute(): string
+    {
+        $placeholder = $this->getAttribute('placeholder');
+        if (empty($placeholder)) {
+            if ($this->without_label) {
+                return (string)$this->label;
+            }
+
+            if ($this->required) {
+                return __('messages.require');
+            }
+        }
+
+        return (string)$placeholder;
+    }
+}
---
+++ tests/InputTextTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace LLkumaLL\FormView\Tests;
+
+use LLkumaLL\FormView\InputText;
+use PHPUnit\Framework\TestCase;
+
+class InputTextTest extends TestCase
+{
+    /**
+     * type属性取得テスト
+     * 
+     * @return void
+     */
+    public function testGetType(): void
+    {
+        $ins = new InputText;
+        $this->assertEquals('text', $ins->getTypeAttribute());
+        $this->assertEquals('text', $ins->type);
+    }
+
+    /**
+     * プレースホルダー属性取得テスト
+     * 
+     * @return void
+     */
+    public function testGetPlaceholderAttribute(): void
+    {
+        $label = 'label text';
+        $placeholder = 'placeholder text';
+        $ins = new InputText;
+        $ins->label = $label;
+
+        $this->assertEmpty($ins->getPlaceholderAttribute());
+        $this->assertEmpty($ins->placeholder);
+
+        // without_label == true && required == false
+        $ins->without_label = true;
+        $this->assertEquals($label, $ins->getPlaceholderAttribute());
+        $this->assertEquals($label, $ins->placeholder);
+
+        // witout_lavel == true && required == true
+        $ins->required = true;
+        $this->assertEquals($label, $ins->getPlaceholderAttribute());
+        $this->assertEquals($label, $ins->placeholder);
+
+        // without_lavel == false && require == true
+        $ins->without_label = false;
+        $this->assertEquals('messages.require', $ins->getPlaceholderAttribute());
+        $this->assertEquals('messages.require', $ins->placeholder);
+
+        // placeholder登録あり
+        $ins->placeholder = $placeholder;
+        $this->assertEquals($placeholder, $ins->getPlaceholderAttribute());
+        $this->assertEquals($placeholder, $ins->placeholder);
+    }
+}
